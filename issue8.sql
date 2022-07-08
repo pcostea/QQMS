9cad6e413d8e66856d8b501c011ac2dd6c8b6a3a
otellopcs <otello.postelnicu@crystal-system.eu>
2022-07-08 14:16:58 +0300

    issue #8 - create Product table

---
+++ issue8.sql
@@ -0,0 +1,15 @@
+-- liquibase formatted sql changeLogId:e3dbe930-ca63-4ef1-9162-d68033390fc6
+
+--changeset Otello:1 labels:issue8 context:create-product
+
+create table product (
+    uid serial primary key not null,
+    constraint product_pkey
+        foreign key(corporation_id)
+            references corporation(uid),
+    name varchar(50) not null,
+    short_name varchar(25),
+    timestamp timestamp,
+    valid_from timestamp,
+    valid_until timestamp
+)
