b074a49990cd9d855644cdf25060abe0677c5b08
ToniSQL <alex.marius@yahoo.com>
2022-07-09 10:21:45 +0300

    Corporation table fix corrected

--- issue4.sql
+++ issue4.sql
@@ -1,12 +1,12 @@
 --liquibase formatted sql
 
---changeset other.dev:20 labels:example-label context:example-context
+--changeset other.dev:22 labels:Issue4 Correction context:Issue4 - Corporation table correction
 --comment: example comment
 
 create table Corporation (
-    uid int primary key, --auto_increment not null,
+    uid serial primary key, --auto_increment not null,
     name varchar(50) not null,
-    timestamp date not null,
+    timestamp timestamp not null,
     valid_from date not null,
     valid_until date not null
 )
