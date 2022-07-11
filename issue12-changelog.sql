b84d2bc98f36fa2138944ea03d6ad882dd50975a
dragos-constantin-stoica <dragos.constantin.stoica@outlook.com>
2022-07-11 13:57:21 +0200

    closes #12

--- issue12-changelog.sql
+++ issue12-changelog.sql
@@ -1,13 +1,18 @@
 --liquibase formatted sql
 /* https://www.liquibase.org/documentation/sql_format.html */
 
---changeset dragos-constantin-stoica:12001 labels:create-transation_value context:issue12
---comment: TODO add product_id column fk when product table is deployed
-/* Insert SQL change objects here */
+--changeset dragos-constantin-stoica:12002 labels:delete-transation_value context:issue12
+DROP TABLE transaction_value;
+
+--changeset dragos-constantin-stoica:12003 labels:new-vesrion-transation_value context:issue12
 create table transaction_value (
+    product_id integer not null,
     uid serial primary key not null,
     value double precision not null,
     currency char(3),
-    timestamp timestamp
+    timestamp timestamp,
+    constraint product_fkey
+        foreign key(product_id)
+            references product(uid)
 )
 --rollback DROP TABLE transaction_value;
